4a08c53b37e5bdeb2ae49e2e728bfde9f2127582
SuCicada <pengyifu@gmail.com>
2021-08-08 23:06:48 +0800

    c run

--- linux/kernel/main.c
+++ linux/kernel/main.c
@@ -1,5 +1,10 @@
 #include<header.h>
+void io_hlt(void);
+
 void bootmain(void){
 //注意这里的函数名字为bootmain，因为在entry.S中设定的入口名字也是bootmain，两者要保持一致
-	while(1);
+fin:
+	// io_hlt();
+	goto fin;
+	// while(1);
 }
