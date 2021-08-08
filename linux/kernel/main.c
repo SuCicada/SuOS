3226b2b4a59264df6f96cf450c619e8135ecf8d2
SuCicada <pengyifu@gmail.com>
2021-08-09 00:24:14 +0800

    extern function

--- linux/kernel/main.c
+++ linux/kernel/main.c
@@ -1,10 +1,10 @@
 #include<header.h>
-void io_hlt(void);
+extern void io_hlt(void);
 
 void bootmain(void){
 //注意这里的函数名字为bootmain，因为在entry.S中设定的入口名字也是bootmain，两者要保持一致
 fin:
-	// io_hlt();
+	io_hlt();
 	goto fin;
 	// while(1);
 }
