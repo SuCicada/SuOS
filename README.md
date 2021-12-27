297fcf1195741fa94c5353cf9a3a07ae77ef3faa
SuCicada <you@example.com>
2021-12-27 10:36:24 +0800

    makefile

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ https://github.com/pollenjp/myHariboteOS
 
 https://github.com/cherishsir/ubuntu230os
 https://github.com/zchrissirhcz/osask-linux
+
+https://shouzo.github.io/2021/05/18/30OS_day4/
+http://takeisamemo.blogspot.com/2014/09/os30os-3-4.html
+http://takeisamemo.blogspot.com/2014/09/os30os-3-4.html
--- linux/test/test.c
+++ linux/test/test.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include "../kernel/utils.c"
+int main() {
+    char s[100];
+    int len;
 
-int main(){
-    char* a[3][2] = {
-        {"123","345"},
-        {"123","nihoa"},
-        {"345","nihoa"}
-    };
-    for(int i=0;i<3;i++){
-        for(int j=0;j<2;j++){
-            printf(" %s",a[i][j]);
-        }
-        printf("\n");
-    }
-
-	char msg[] = "1234";
-    printf("%d\n",sizeof(msg));
+    su_sprintf(s,"nihoa: %d |",10); 
+    // su_sprintf(s,"nihoa: |"); 
+    printf("%s|\n|", s);
+    printf("=====\n");
+    return 0;
 
 }
